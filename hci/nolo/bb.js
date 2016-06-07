fcf2af754149d7273342f1d881b0c9380da21f16
ckcks12 <wyoducc@gmail.com>
2016-06-07 14:07:50 +0900

    auto load and fetch

--- hci/nolo/bb.js
+++ hci/nolo/bb.js
@@ -2,55 +2,46 @@ var Place = Backbone.Model.extend(
 {
     defaults:
     {
-        title: "t-",
-        content: "-",
+        title: "",
+        content: "",
         image_url: ""
     },
     parse: function(d)
     {
-        var tmp = {};
-        tmp.title = d.title;
-        tmp.content = "aa";
-        tmp.image_url = d.firstimage !== undefined ? d.firstimage : "http://www.realestatemarketingdude.com/wp-content/uploads/2015/01/questionmark-300x200.png";
+        if( this.image_url === undefined || this.image_url == "" )
+        {
+            d.image_url = d.firstimage !== undefined ? d.firstimage : "http://www.realestatemarketingdude.com/wp-content/uploads/2015/01/questionmark-300x200.png";
+        }
 
-        return tmp;
+        Backbone.ajax(
+        {
+            url: window.api.detailIntro(10, 1, d.contentid, d.contenttypeid, "Y"),
+            success: (k) =>
+            {
+                k = k.response.body.items.item;
+                _.extend(this.attributes, k);
+                if( k.playtime !== undefined )
+                {
+                    this.set("content", k.playtime);
+                }
+            }
+        });
+
+        return d;
     }
 });
 
 var PlaceCol = Backbone.Collection.extend(
 {
     model: Place,
+    page_no: 1,
     url: function()
     {
-        this.page_no++;
-        return "http://api.visitkorea.or.kr/openapi/service/rest/KorService/areaBasedList" 
-                + "?ServiceKey=tM%2BbwWQD383lWLKYsJUEFDxC2NoGAvT5rTymTZpfaknMV4FYXam%2FzA3TsqAp%2FkfPbM0tlV%2BaLsut%2B8TigqmgSg%3D%3D"
-                + "&MobileOS=ETC&MobileApp=Nolo&_type=json"
-                + "&numOfRows=" + this.num_of_row
-                + "&pageNo=" + this.page_no
-                + "&arrange=" + this.arrange
-                + "&listYN=" + this.list_yn
-                + "&contentTypeId=" + this.content_type_id
-                + "&areaCode=" + this.area_code
-                + "&signguCode=" + this.signgu_code
-                + "&cat1=" + this.cat1
-                + "&cat2=" + this.cat2
-                + "&cat3=" + this.cat3;
+        return window.api.areaBasedList(10, this.page_no++, "B", "Y");
     },
-    num_of_row: 10,
-    page_no: 0,
-    arrange: "B",
-    list_yn: "Y",
-    content_type_id: "",
-    area_code: "",
-    signgu_code: "",
-    cat1: "",
-    cat2: "",
-    cat3: "",
     parse: function(d)
     {
         return d.response.body.items.item;
-        //console.log(d);
     }
 });
     
@@ -63,7 +54,6 @@ var PlaceView = Backbone.View.extend(
     },
     initialize: function()
     {
-        //this.el = $("#SearchResult").append("<div></div>").children().last();
         this.model.on('change', this.render, this);
         this.render();
     },
@@ -78,7 +68,6 @@ var PlaceView = Backbone.View.extend(
     },
     mousemove: function()
     {
-        this.model.collection.page_no++;
         load();
     }
 });
@@ -88,14 +77,23 @@ var PlaceView = Backbone.View.extend(
 
 
 
-
-var place = new Place;
-var place2 = new Place;
 var place_col = new PlaceCol();
-//var place_view = new PlaceView({model: place});
-//var place_view2 = new PlaceView({model: place2});
 window.view_arr = [];
-load();
+
+$(window).scroll(function() {
+    if( window.scroll_detect === undefined )
+    {
+        window.scroll_detect = true;
+    }
+    if($(window).scrollTop() + $(window).height() >= $(document).height() - 1
+        && window.scroll_detect) {
+        load();
+        window.scroll_detet = false;
+        setTimeout(()=>window.scroll_detect = true, 2000);
+    }
+});
+
+//load();
 function load()
 {
     place_col.fetch(
@@ -105,7 +103,6 @@ function load()
         {
             place_col.each(function(m)
             {
-                console.log(m);
                 window.view_arr[window.view_arr.length] = new PlaceView({model: m});
             });
         }
